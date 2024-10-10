cf51b8884bd25c869e86bde3b2106b90eaf43181
Akshat Tiwari <atiwari5@ucsc.edu>
2024-10-10 13:20:17 -0700

    Redeploy

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-# Vercel-specific: Make sure Flask can run as a serverless function
-def handler(event, context):
-    return app(event, context)
 
 if __name__ == '__main__':
     app.run(debug=True)
